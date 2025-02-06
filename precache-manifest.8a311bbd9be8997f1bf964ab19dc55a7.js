6caa9853839e48aca14589004a94e100fae5ac3a
mohtadiib <mohtadyp@gmail.com>
2025-02-06 12:23:34 +0200

    first commit 2

---
+++ precache-manifest.8a311bbd9be8997f1bf964ab19dc55a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0bfb7c4e84d03f451685deea1c7f33",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4dad3bacc252641fc543",
+    "url": "./static/css/2.903596ff.chunk.css"
+  },
+  {
+    "revision": "218b95b6295d6ff1ffad",
+    "url": "./static/css/main.a23c4c7b.chunk.css"
+  },
+  {
+    "revision": "4dad3bacc252641fc543",
+    "url": "./static/js/2.c5c376a5.chunk.js"
+  },
+  {
+    "revision": "d18f7a2e1b52490df46c77a3682f8987",
+    "url": "./static/js/2.c5c376a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218b95b6295d6ff1ffad",
+    "url": "./static/js/main.9c9af966.chunk.js"
+  },
+  {
+    "revision": "9d7924f4e7776a1e4bf7",
+    "url": "./static/js/runtime-main.a136ab0f.js"
+  },
+  {
+    "revision": "c398871b85c6bcecbee391b4b247d38f",
+    "url": "./static/media/Shamel-book.c398871b.ttf"
+  },
+  {
+    "revision": "74ea5c31efe859c4a757359a57710ad3",
+    "url": "./static/media/bankak.74ea5c31.png"
+  },
+  {
+    "revision": "c3a90dc9c757ca4c978c0c24c8b880c0",
+    "url": "./static/media/header.c3a90dc9.png"
+  },
+  {
+    "revision": "7227ebc3bf7b16a79f59f4c7d5dd9e44",
+    "url": "./static/media/signature.7227ebc3.png"
+  }
+]);
